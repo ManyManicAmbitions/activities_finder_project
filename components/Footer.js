bcd18c7df851a5709e9df6ecccc7a9565632a642
ManyManicAmbitions <russelltyrel9@gmail.com>
2025-08-14 10:43:51 -0400

    Styled footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
     return(
-        <footer>
-            <p>&copy; 2025 Activities Finder</p>
+        <footer className="text-center bg-black p-2 mt-4">
+            <p className="text-sm text-gray-500">
+                &copy; 2025 Activities Finder
+            </p>
         </footer>
     );
 }
